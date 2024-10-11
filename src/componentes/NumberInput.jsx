fc10633966be3b301e094964b1b20f9dce90327b
nathernandez1189 <123783069+nathernandez1189@users.noreply.github.com>
2024-10-11 05:29:11 -0500

    ultimo arreglo de celular

--- src/componentes/NumberInput.jsx
+++ src/componentes/NumberInput.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../App.css';  
+import '../App.css';
 
 const NumberInput = ({ label, name, value, handleChange }) => {
   return (
     <div className="number-input">
-      <label htmlFor={name}>
-        {label}
-        <input
-          type="number"
-          id={name}
-          name={name}
-          value={value}
-          onChange={handleChange}
-        />
-      </label>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type="number"
+        id={name}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        onFocus={(e) => e.target.select()} 
+    />
     </div>
   );
 };
